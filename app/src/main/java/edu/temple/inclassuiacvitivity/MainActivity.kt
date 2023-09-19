33b4015abc31d31d9bfafbba6fdd91b8d97e826d
Kevin Jarema <tun48388@temple.edu>
2023-09-19 15:08:08 -0400

    Populated an array, changed the text size to fit the array size, changed the adapter to display items from the array in spinner.

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,14 +16,30 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* Step 1: Populate this array */
-        //val numberArray = Array Of Numbers
+        val numberArray = Array(20) {(it+1) *2}
 
         /* Step 2: Create adapter to display items from array in Spinner */
-        //spinner.adapter = ArrayAdapter...
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item,numberArray)
 
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object : OnItemSelectedListener {
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+            val selectedNumber = numberArray[position]
+            displayTextView.textSize = selectedNumber.toFloat() // Set text size based on the selected number
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>) {
+            // Handle nothing selected if needed
+        }
+
+
+
+
+
+
+        }
 
     }
 }
